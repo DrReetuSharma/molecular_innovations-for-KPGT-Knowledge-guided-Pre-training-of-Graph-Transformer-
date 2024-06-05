01b523dbea2cca12e69c9c167229e2036f5c2486
DrReetuSharma <162538254+DrReetuSharma@users.noreply.github.com>
2024-06-05 11:40:06 +0530

    Create __init__.py

---
+++ src/data/descriptors/__init__.py
@@ -0,0 +1,9 @@
+#the file to indicate that this folder is a package
+
+import logging
+from .DescriptorGenerator import *
+from .rdDescriptors import *
+from .rdNormalizedDescriptors import *
+    
+
+        
